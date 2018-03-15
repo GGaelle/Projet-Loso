1679565c52f0a584aefc6fa2ee022dbe68d58915
Gaëlle <37183390+GGaelle@users.noreply.github.com>
2018-03-15 21:56:25 +0100

    JS pour auto padding to header

--- js/custom.js
+++ js/custom.js
@@ -128,7 +128,6 @@ $(document).ready(function (){
             } else {
                 $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
                 
-                
             }
             
         });
@@ -137,7 +136,30 @@ $(document).ready(function (){
         
 });
 
+// add auto padding to header
 
+$(document).ready(function(){
+    'use strict';
+    
+    setInterval(function(){
+        'use strict';
+        
+        var windowHeight = $(window).height();
+        var containerHeight = $(".header-container").height();
+        var padTop = windowHeight - containerHeight;
+        $(".header-container").css({
+           
+            'padding-top': Math.round( padTop / 2) + 'px',
+            'padding-bottom': Math.round( padTop / 2) + 'px'
+            
+        });
+        
+        
+    }, 10)
+    
+    
+    
+});
 
 
 
